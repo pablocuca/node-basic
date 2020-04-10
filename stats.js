5aa0d5c674c953922983c9c0c6e7aec09b5bcd4f
Pablo Custodio Carneiro <pablocustodio@hotmail.com>
2020-04-09 22:52:33 -0300

    finished logger

--- stats.js
+++ stats.js
@@ -1,13 +1,15 @@
 const os = require('os');
+const log = require('./logger')
 
 setInterval(()=>{
     const { freemem, totalmem } = os;
     
     const total = parseInt(totalmem() / 1024 / 1024);
     const mem = parseInt(freemem() / 1024 / 4024);
-    const percents = parseInt(100 - (mem / total) * 100);
+    const percents = parseFloat(100 - (mem / total) * 100).toFixed(2);
     
     const stats = {
+        date: `${new Date().toString()}`,
         free: `${mem} MB`,
         total: `${total} MB`,
         usage: `${percents} %`
@@ -16,4 +18,6 @@ setInterval(()=>{
     console.clear()
     console.log('=== PC STATS ===');
     console.table(stats);
-}, 1000);
+    
+    log(`${JSON.stringify(stats)}\n`);
+}, 3000);
